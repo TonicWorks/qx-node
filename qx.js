2e9e28188e1f35555b9f602bbf4b0695e86b56c4
Steven Dickinson <sd@tonic.works>
2018-07-05 17:28:31 +0100

    Use urllib to parse url if provided. Retain old hostname compatability

--- qx.js
+++ qx.js
@@ -1,27 +1,54 @@
 'use strict';
 
 const _ = require('lodash');
+const urllib = require('url');
 const requestPromise = require('minimal-request-promise');
 const buildQuote = require('./src/build_quote');
 const summariseQuote = require('./src/quote_summariser');
 
+function credentialsToRequestOptions(credentials) {
+
+    if (credentials.url) {
+        const url = urllib.parse(credentials.url);
+        return {
+            hostname: url.hostname,
+            path:     url.pathname,
+            port:     url.port,
+            protocol: url.protocol,
+
+            headers:  {
+                'Content-Type':  'application/json',
+                'Authorization': 'Basic ' + new Buffer(credentials.pass).toString('base64')
+            },
+        }
+    } else {
+        return {
+            hostname: credentials.hostname,
+            path:     '',
+            port:     443,
+
+            headers:  {
+                'Content-Type':  'application/json',
+                'Authorization': 'Basic ' + new Buffer(credentials.pass).toString('base64')
+            },
+        }
+    }
+}
+
 module.exports = {
 
-    addQuote: function(credentials,contact,quoteEntries,sessionId){
+    addQuote:      function (credentials, contact, quoteEntries, sessionId) {
 
-        return buildQuote(contact,quoteEntries)
-            .then(function (body){
+        return buildQuote(contact, quoteEntries)
+            .then(function (body) {
 
                 var options = {
-                    method: 'POST',
-                    hostname: credentials.hostname,
-                    path: '/api/1/quotes.json',
-                    port: 443,
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': "Basic " + new Buffer(credentials.pass).toString("base64")
-                    }
+                    method:  'POST',
                 };
+
+                Object.assign(options, credentialsToRequestOptions(credentials));
+                options.path += '/api/1/quotes.json';
+
                 //optional session id so that qx can reference back to other systems - qx needs to be extended to accept it before we can start sending it
                 /*if(!_.isEmpty(sessionId)){
                     body.sessionId = sessionId;
@@ -36,68 +63,64 @@ module.exports = {
                 return requestPromise(options);
 
             }, function (err) {
-                console.error('error building quote',err);
-                return {success:false};
+                console.error('error building quote', err);
+                return { success: false };
             })
             .then(function (response) {
                 console.log('quote is ok')
                 console.log('response is ', response.body);
                 let body = JSON.parse(response.body);
                 //console.log('quote is ', body);
-                if(_.get(body, ['quoteId']) && _.get(body, ['hash'])){
+                if (_.get(body, ['quoteId']) && _.get(body, ['hash'])) {
                     let resp = {
-                        success : true,
-                        quoteId : body.quoteId,
-                        hash : body.hash
+                        success: true,
+                        quoteId: body.quoteId,
+                        hash:    body.hash
                     }
                     return Promise.resolve(resp);
                 }
-                else{
-                    return {success:false};
+                else {
+                    return { success: false };
                 }
             }, function (error) {
                 console.log('got error', error.body, error.headers, error.statusCode, error.statusMessage);
-                return {success:false};
+                return { success: false };
             })
-            /*.then(function(res) {
-                return res;
-            }, function (error) {
-                console.log('got error', error);
-                return {success:false};
-            });*/
+        /*.then(function(res) {
+            return res;
+        }, function (error) {
+            console.log('got error', error);
+            return {success:false};
+        });*/
     },
-    retrieveQuote: function(credentials,hash){
+    retrieveQuote: function (credentials, hash) {
 
-            var options = {
-                method: 'GET',
-                hostname: credentials.hostname,
-                path: '/api/1/quotes/' + hash + '.json',
-                port: 443,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': "Basic " + new Buffer(credentials.pass).toString("base64")
-                }
-            };
+       let options = {
+            method: 'GET',
+        };
 
-            return requestPromise(options)
+        Object.assign(options, credentialsToRequestOptions(credentials));
+        options.path += '/api/1/quotes/' + hash + '.json';
+
+        return requestPromise(options)
             .then(function (response) {
                 let body = JSON.parse(response.body);
                 //console.log('quote is ', body);
-                if(_.get(body, ['quoteId'])){
+                if (_.get(body, ['quoteId'])) {
                     return summariseQuote(body);
                 }
-                else{
-                    return {success:false};
+                else {
+                    return { success: false };
                 }
             }, function (error) {
                 console.log('got error', error.body, error.headers, error.statusCode, error.statusMessage);
-                return {success:false};
+                return { success: false };
             })
-            .then(function(res) {
+            .then(function (res) {
                 return res;
             }, function (error) {
                 console.log('got error', error);
-                return {success:false};
+                return { success: false };
             });
     }
 };
